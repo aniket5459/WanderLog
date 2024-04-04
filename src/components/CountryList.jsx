0640a314aaac49532f9cd2faa357923179fb09e4
Aniket <aniketkesari007@gmail.com>
2024-04-05 00:48:03 +0530

    First commit

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,63 @@
+/* eslint-disable react/prop-types */
+import styles from "./CountryList.module.css";
+
+import Spinner from "./Spinner";
+
+import CountryItem from "./CountryItem";
+
+import Message from "./Message";
+import { useCities } from "../context/CitiesContext";
+
+
+
+function CountryList() {
+    const { cities, isLoading } = useCities()
+    if (isLoading) return <Spinner />;
+
+
+
+    if (!cities.length)
+
+        return (
+
+            <Message message="Add your first city by clicking on a city on the map" />
+
+        );
+
+
+
+    const countriesUnique = new Set(
+
+        cities.map((city) =>
+
+            JSON.stringify({ country: city.country, emoji: city.emoji })
+
+        )
+
+    );
+
+    const countries = [...countriesUnique].map((each) => JSON.parse(each));
+
+
+
+    console.log(countries);
+
+    return (
+
+        <ul className={styles.countryList}>
+
+            {countries.map((country) => {
+
+                return <CountryItem country={country} key={country.country} />;
+
+            })}
+
+        </ul>
+
+    );
+
+}
+
+
+
+export default CountryList;
